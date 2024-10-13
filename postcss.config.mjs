1cf7728dce5c0412e9f1674799cbbceeb5a5943a
Haris R <hredzic01@gmail.com>
2024-10-13 17:40:18 -0500

    more daisyui issues

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,12 @@
 import autoprefixer from 'autoprefixer';
+import daisyui from 'daisyui';
 
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    daisyui: {},
   },
 };
 
