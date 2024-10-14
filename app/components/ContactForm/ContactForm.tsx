d94ee1c9043674a56a7d085c0bd53f05eb0300e3
Haris R <hredzic01@gmail.com>
2024-10-14 15:57:22 -0500

    remove icons from success message

--- app/components/ContactForm/ContactForm.tsx
+++ app/components/ContactForm/ContactForm.tsx
@@ -114,19 +114,6 @@ const ContactForm: React.FC<ContactFormProps> = ({ siteKey }) => {
     <div className="mt-8 w-full max-w-lg">
       {isSubmitted && (
         <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 flex-shrink-0 stroke-current"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 13l4 4L19 7"
-            />
-          </svg>
           <span>
             Thank you for reaching out! I&apos;ll get back to you as soon as
             possible :)
