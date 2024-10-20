fde1c682769acf953bf5fe608ce67b700c5ddec1
Haris R <hredzic01@gmail.com>
2024-10-20 11:34:07 -0500

    update sitemap domains

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,31 +3,31 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-        url: 'https://hredz.pages.dev',
+        url: 'https://hredz.com',
         lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 1,
     },
     {
-        url: 'https://hredz.pages.dev/about',
+        url: 'https://hredz.com/about',
         lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 0.9,
     },
     {
-        url: 'https://hredz.pages.dev/my-work',
+        url: 'https://hredz.com/my-work',
         lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 0.8,
     },
     {
-        url: 'https://hredz.pages.dev/blog',
+        url: 'https://hredz.com/blog',
         lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 0.7,
     },
     {
-        url: 'https://hredz.pages.dev/contact',
+        url: 'https://hredz.com/contact',
         lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 0.5,
